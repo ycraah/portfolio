ff1db589f22c05c72a04aff3934762f339892fdb
김예찬 <ycraah@gamil.com>
2025-02-27 13:42:19 +0900

    js 코드 위치 변경

--- script/pageLoading.js
+++ script/pageLoading.js
@@ -1,5 +1,16 @@
+/* 로딩 완료 시 로딩 화면 제거 */
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
     document.querySelector(".loading-screen").style.top = "-100%";
   }, 5000);
 });
+
+/* 로딩창에서 휠 불가능 */
+const loadingPage = document.querySelector(".loading-screen");
+loadingPage.addEventListener(
+  "wheel",
+  function (event) {
+    event.preventDefault();
+  },
+  { passive: false }
+);
