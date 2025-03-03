58466649981b7b77cc66f1571dc3d26bdb5236f3
김예찬 <ycraah@gamil.com>
2025-03-03 22:35:55 +0900

    로딩 완료 load로 변경

--- script/pageLoading.js
+++ script/pageLoading.js
@@ -1,15 +1,16 @@
+const loadingPage = document.querySelector(".loading-screen");
+
 /* 로딩 완료 시 로딩 화면 제거 */
-document.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("load", function () {
   setTimeout(function () {
-    document.querySelector(".loading-screen").style.top = "-100%";
+    loadingPage.style.top = "-100%";
     setTimeout(function () {
-      document.querySelector(".loading-screen").style.opacity = "0";
+      loadingPage.style.opacity = "0";
     }, 500);
   }, 5000);
 });
 
 /* 로딩창에서 휠 불가능 */
-const loadingPage = document.querySelector(".loading-screen");
 loadingPage.addEventListener(
   "wheel",
   function (event) {
